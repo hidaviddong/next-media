8d75ac351c6cb61f7a923654e0b7f9fd1942cfc1
David Dong <hidaviddong@gmail.com>
2025-09-15 14:20:01 +0800

    chore: update

--- apps/api/src/routes/tests/health.test.ts
+++ apps/api/src/routes/tests/health.test.ts
@@ -1,8 +1,6 @@
-import { Hono } from "hono";
 import { testClient } from "hono/testing";
-import { describe, it, test, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 import app from "../../index.js";
-import type { Variables } from "../../type.js";
 
 describe("health", () => {
   const client = testClient(app);
